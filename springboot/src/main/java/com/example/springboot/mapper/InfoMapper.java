f13a5a7ba2afa642af0bbfaae1b3635437dd91d5
zluyuan <87009805+zluyuan@users.noreply.github.com>
2022-11-10 18:33:08 +0800

    Delete springboot directory

--- springboot/src/main/java/com/example/springboot/mapper/InfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.springboot.entity.Info;
-
-public interface InfoMapper extends BaseMapper<Info> {
-}
